1f0b3a6df6126fb7995513c2e84762834becb71c
Patrick Kuang <pkuang@ucsb.edu>
2021-05-26 14:46:33 -0700

    connect handler

--- sqlite/java/connect/net/sqlitetutorial/Connect.java
+++ sqlite/java/connect/net/sqlitetutorial/Connect.java
@@ -1,20 +1,15 @@
 package net.sqlitetutorial;
 import java.sql.*;
 
-/**
- *
- * @author sqlitetutorial.net
- */
 public class Connect {
-     /**
-     * Connect to a sample database
-     */
-    public static void connect(){
-        Connection conn = null;
+    
+    private Connection conn;
+
+    Connect() throws SQLException {
         try {
             //db parameters
             // DriverManager.registerDriver(new oracle.jdbc.OracleDriver());	
-            String url = "jdbc:sqlite:/Users/moshehaiem/Documents/CS174A/Project/Project/sqlite/db/chinook.db";
+            String url = "jdbc:sqlite:/Users/patrickkuang/cs174a_project/sqlite/db/chinook.db";
             // create a connection to the database
             conn = DriverManager.getConnection(url);
             
@@ -32,12 +27,11 @@ public class Connect {
             }
         }
     }
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        connect();
-    }
+
+    public Connection getConnection()
+	{
+		return conn;
+	}
 }
 
 
